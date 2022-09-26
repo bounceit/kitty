32023ac28bc7e7e84c1998f7aa3ed21c01d639df
Oleksandr <bounceit1221@gmail.com>
2022-09-26 15:37:24 +0300

    Create navigation_bloc.dart

---
+++ lib/blocs/bloc_navigations/navigation_bloc.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'navigation_event.dart';
+import 'navigation_state.dart';
+
+class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
+  NavigationBloc()
+      : super(
+          const NavigationState(),
+        ) {
+    on<NavigateMenu>((
+      event,
+      emit,
+    ) {
+      emit(
+        state.copyWith(
+          status: NavigationStateStatus.menu,
+          currentIndex: event.menuIndex,
+          route: event.route,
+        ),
+      );
+    });
+
+    on<NavigateTab>((
+      event,
+      emit,
+    ) {
+      emit(
+        state.copyWith(
+          status: NavigationStateStatus.tab,
+          currentIndex: event.tabIndex,
+          route: event.route,
+        ),
+      );
+    });
+  }
+}
