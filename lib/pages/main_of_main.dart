5c3c75fbbcb05c90935d1665ff977bc7baa4572a
Oleksandr <bounceit1221@gmail.com>
2022-09-26 15:37:42 +0300

    Create main_of_main.dart

---
+++ lib/pages/main_of_main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kitty/blocs/bloc_navigations/navigation_bloc.dart';
+import 'package:kitty/pages/main_pages/home_page.dart';
+import 'package:kitty/pages/main_pages/report_page/report_page.dart';
+import 'package:kitty/pages/main_pages/settings_page.dart';
+
+import '../blocs/bloc_navigations/navigation_event.dart';
+import '../blocs/bloc_navigations/navigation_state.dart';
+import '../routes/routes.dart';
+import '../widgets/navigation/bottom_nav_bar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+  static const routeName = '/main';
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  static const List<String> _pages = [
+    ReportPage.routeName,
+    HomePage.routeName,
+    SettingsPage.routeName,
+  ];
+
+  static final GlobalKey<NavigatorState> _navigatorKey =
+      GlobalKey<NavigatorState>();
+  void _onSelectMenu(String route) {
+    if (_navigatorKey.currentState != null) {
+      _navigatorKey.currentState!.pushNamedAndRemoveUntil(
+        route,
+        (_) => false,
+      );
+    }
+  }
+
+  void _onSelectTab(String route) {
+    if (_navigatorKey.currentState != null) {
+      _navigatorKey.currentState!.pushNamedAndRemoveUntil(
+        route,
+        (route) => false,
+      );
+    }
+  }
+
+  Future<bool> _onWillPop() async {
+    final bool maybePop = await _navigatorKey.currentState!.maybePop();
+
+    return !maybePop;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NavigationBloc>(
+          create: (_) => NavigationBloc(),
+        ),
+      ],
+      child: BlocConsumer<NavigationBloc, NavigationState>(
+        listener: (_, state) {
+          if (state.status == NavigationStateStatus.menu) {
+            _onSelectMenu(state.route);
+          }
+
+          if (state.status == NavigationStateStatus.tab) {
+            _onSelectTab(state.route);
+          }
+        },
+        builder: (context, state) {
+          return WillPopScope(
+            onWillPop: _onWillPop,
+            child: Scaffold(
+              body: Navigator(
+                key: _navigatorKey,
+                initialRoute: ReportPage.routeName,
+                onGenerateRoute: AppRouter.generateRoute,
+              ),
+              bottomNavigationBar: BottomNavBar(
+                currentTab: state.currentIndex,
+                onSelect: (int index) {
+                  if (state.currentIndex != index) {
+                    context.read<NavigationBloc>().add(
+                          NavigateTab(
+                            tabIndex: index,
+                            route: _pages[index],
+                          ),
+                        );
+                  }
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
