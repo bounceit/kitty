cb67a4aee2a0327ea1a919a9041ed045c1d1795e
Oleksandr <bounceit1221@gmail.com>
2022-09-26 15:37:27 +0300

    Create navigation_event.dart

---
+++ lib/blocs/bloc_navigations/navigation_event.dart
@@ -0,0 +1,21 @@
+abstract class NavigationEvent {}
+
+class NavigateMenu extends NavigationEvent {
+  NavigateMenu({
+    required this.menuIndex,
+    required this.route,
+  });
+
+  final int menuIndex;
+  final String route;
+}
+
+class NavigateTab extends NavigationEvent {
+  NavigateTab({
+    required this.tabIndex,
+    required this.route,
+  });
+
+  final int tabIndex;
+  final String route;
+}
