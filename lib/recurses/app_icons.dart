233b0e762cf41021cc1b80c4fb0323a054e6f27c
Oleksandr <bounceit1221@gmail.com>
2022-09-26 15:38:05 +0300

    Create app_icons.dart

---
+++ lib/recurses/app_icons.dart
@@ -0,0 +1,7 @@
+class AppIcons {
+  const AppIcons._();
+  static const String path = 'assets/icons';
+  static const String report = '$path/Vector.png';
+  static const String home = '$path/Vector (1).png';
+  static const String settings = '$path/Vector (2).png';
+}
