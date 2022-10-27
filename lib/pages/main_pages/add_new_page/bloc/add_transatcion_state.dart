5d8d21e45160167913bc1de17214368926646830
Oleksandr <bounceit1221@gmail.com>
2022-10-27 12:39:57 +0300

    Create add_transatcion_state.dart

---
+++ lib/pages/main_pages/add_new_page/bloc/add_transatcion_state.dart
@@ -0,0 +1,10 @@
+part of 'add_transatcion_bloc.dart';
+
+abstract class AddTransatcionState extends Equatable {
+  const AddTransatcionState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AddTransatcionInitial extends AddTransatcionState {}
