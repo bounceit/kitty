76c577080749ecb5f81001edc9ead7390100534a
Oleksandr <bounceit1221@gmail.com>
2022-10-27 12:40:06 +0300

    Create drop_type.dart

---
+++ lib/pages/main_pages/add_new_page/widgets/drop_type.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+
+// class DownDropButton extends StatefulWidget {
+//   const DownDropButton({Key? key}) : super(key: key);
+
+//   @override
+//   State<DownDropButton> createState() => _DropdownButtonState();
+// }
+
+// class _DropdownButtonState extends State<DownDropButton> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
+//   }
+// }
