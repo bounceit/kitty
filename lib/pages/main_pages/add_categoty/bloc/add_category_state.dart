e1d1252bcab8e8125f1b6cdcbf43b3693521ac86
Oleksandr <bounceit1221@gmail.com>
2022-10-27 12:39:26 +0300

    Create add_category_state.dart

---
+++ lib/pages/main_pages/add_categoty/bloc/add_category_state.dart
@@ -0,0 +1,10 @@
+part of 'add_category_bloc.dart';
+
+abstract class AddCategoryState extends Equatable {
+  const AddCategoryState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class AddCategoryInitial extends AddCategoryState {}
