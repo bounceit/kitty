d8f127cebd7922d695ac39949054c2f8813395b0
Oleksandr <bounceit1221@gmail.com>
2022-10-27 12:39:49 +0300

    Create add_transatcion_bloc.dart

---
+++ lib/pages/main_pages/add_new_page/bloc/add_transatcion_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:kitty/models/transaction.dart';
+import 'package:kitty/repository/transaction_repository.dart';
+
+part 'add_transatcion_event.dart';
+part 'add_transatcion_state.dart';
+
+class AddTransatcionBloc extends Bloc<AddTransatcionEvent, void> {
+  AddTransatcionBloc() : super(AddTransatcionInitial()) {
+    on<AddTransatcionEvent>((event, emit) {
+      switch (event.type) {
+        case TransactionType.income:
+          final kittys = TransactionModel(
+            id: Random().nextInt(999999),
+            title: event.title,
+            amount: event.amount,
+            type: 'Income',
+          );
+
+          // DataHelper.db.income();
+          KittyRepository().addKitty(kittys);
+          break;
+        case TransactionType.expense:
+          final kittys = TransactionModel(
+            id: Random().nextInt(999999),
+            title: event.title,
+            amount: event.amount,
+            type: 'Expensive',
+          );
+
+          // DataHelper.db.expense();
+          KittyRepository().addKitty(kittys);
+          break;
+      }
+    });
+  }
+}
