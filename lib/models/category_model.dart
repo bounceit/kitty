264507791463072e739f638f18dd7d625eb3186e
Oleksandr <bounceit1221@gmail.com>
2022-10-29 12:14:34 +0300

    Update category_model.dart

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -2,6 +2,7 @@ class CategoryModel {
   factory CategoryModel.fromMap(Map<String, dynamic> json) => CategoryModel(
         category: json['categoryName'] ?? 0,
         icon: json['categoryIcon'] ?? 0,
+        id: json['id'] ?? 0,
       );
   CategoryModel({
     this.category = '',
@@ -13,6 +14,7 @@ class CategoryModel {
   String icon;
 
   Map<String, dynamic> toMap() => {
+        'id': id,
         'categoryName': category,
         'categoryIcon': icon,
       };
