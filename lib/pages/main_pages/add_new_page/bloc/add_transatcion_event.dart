07b84400e8375cdf04414573e4539c225abe896a
Oleksandr <bounceit1221@gmail.com>
2022-10-29 12:15:18 +0300

    Update add_transatcion_event.dart

--- lib/pages/main_pages/add_new_page/bloc/add_transatcion_event.dart
+++ lib/pages/main_pages/add_new_page/bloc/add_transatcion_event.dart
@@ -2,16 +2,26 @@ part of 'add_transatcion_bloc.dart';
 
 class AddTransatcionEvent extends Equatable {
   const AddTransatcionEvent({
+    required this.categoryName,
+    required this.categoryIcon,
     required this.title,
     required this.amount,
     required this.type,
   });
   final String title;
+  final String categoryName;
+  final String categoryIcon;
   final int amount;
   final TransactionType type;
 
   @override
-  List<Object> get props => [title, amount, type];
+  List<Object> get props => [
+        title,
+        amount,
+        type,
+        categoryIcon,
+        categoryName,
+      ];
 }
 
 enum TransactionType { income, expense }
