04accc054602cdeb19292e8c3d570d538fb875df
Oleksandr <bounceit1221@gmail.com>
2022-10-31 15:55:57 +0300

    Update home_page_bloc.dart

--- lib/pages/main_pages/home_page/bloc/home_page_bloc.dart
+++ lib/pages/main_pages/home_page/bloc/home_page_bloc.dart
@@ -6,8 +6,6 @@ part 'home_page_state.dart';
 
 class HomePageBloc extends Bloc<HomePageEvent, HomePageState> {
   HomePageBloc() : super(HomePageInitial()) {
-    on<HomePageEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomePageEvent>((event, emit) {});
   }
 }
