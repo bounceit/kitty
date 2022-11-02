3e48bd50c55e716e10c54c29438686f7d8e640e5
Oleksandr <bounceit1221@gmail.com>
2022-11-02 18:45:11 +0300

    Update add_category_event.dart

--- lib/pages/main_pages/add_categoty/bloc/add_category_event.dart
+++ lib/pages/main_pages/add_categoty/bloc/add_category_event.dart
@@ -1,10 +1,14 @@
 part of 'add_category_bloc.dart';
 
 class AddCategoryEvent extends Equatable {
-  const AddCategoryEvent({required this.category, required this.selectedIcon});
+  const AddCategoryEvent(
+      {required this.category,
+      required this.selectedIcon,
+      required this.selectedColor});
 
   final String category;
   final String? selectedIcon;
+  final int? selectedColor;
   @override
   List<Object> get props => [];
 }
