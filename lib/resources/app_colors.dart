872a9f556aceded16751df89c22f132309837934
Oleksandr <bounceit1221@gmail.com>
2022-11-06 12:53:22 +0300

    Create app_colors.dart

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color appBarAddPage = Color(0xFFF5F5F5);
+  static const Color ball = Color.fromRGBO(255, 205, 210, 1);
+  static const Color car = Color.fromRGBO(200, 230, 201, 1);
+  static const Color city = Color.fromRGBO(248, 187, 208, 1);
+  static const Color coffe = Color.fromRGBO(179, 157, 219, 1);
+  static const Color computer = Color.fromRGBO(248, 187, 208, 1);
+  static const Color education = Color.fromRGBO(179, 229, 252, 1);
+  static const Color food = Color.fromRGBO(178, 235, 242, 1);
+  static const Color fuel = Color.fromRGBO(220, 237, 200, 1);
+  static const Color gratulation = Color.fromRGBO(207, 216, 220, 1);
+  static const Color hand = Color.fromRGBO(215, 204, 200, 1);
+  static const Color happy = Color.fromRGBO(197, 202, 233, 1);
+  static const Color health = Color.fromRGBO(255, 224, 178, 1);
+  static const Color liquor = Color.fromRGBO(230, 238, 156, 1);
+  static const Color money = Color.fromRGBO(255, 236, 179, 1);
+  static const Color pig = Color.fromRGBO(187, 222, 251, 1);
+  static const Color shop = Color.fromRGBO(225, 190, 231, 1);
+  static const Color human = Color.fromRGBO(255, 249, 196, 1);
+}
