e39b2068fc5ac864b95648765e44e9c8b2d9ce1f
Oleksandr <bounceit1221@gmail.com>
2022-11-06 12:53:15 +0300

    Update settings_page.dart

--- lib/pages/main_pages/settings_page/settings_page.dart
+++ lib/pages/main_pages/settings_page/settings_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kitty/pages/main_pages/manage_category/manage_category.dart';
+import 'package:kitty/resources/app_colors.dart';
+import 'package:kitty/resources/app_icons.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -7,7 +11,96 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: AppColors.appBarAddPage,
+        elevation: 0.0,
+        toolbarHeight: MediaQuery.of(context).size.height * 0.22,
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Card(
+          elevation: 0.0,
+          child: ListTile(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                ManageCategory.routeName,
+              );
+            },
+            trailing: SvgPicture.asset(AppIcons.go),
+            leading: SvgPicture.asset(AppIcons.categories),
+            title: const Text(
+              'Manage Categories',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 0.0,
+          child: ListTile(
+            trailing: SvgPicture.asset(AppIcons.go),
+            leading: SvgPicture.asset(AppIcons.pdf),
+            title: const Text(
+              'Export to PDF',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 0.0,
+          child: ListTile(
+            trailing: SvgPicture.asset(AppIcons.go),
+            leading: SvgPicture.asset(AppIcons.translate),
+            title: const Text(
+              'Choose Language',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 0.0,
+          child: ListTile(
+            trailing: SvgPicture.asset(AppIcons.go),
+            leading: SvgPicture.asset(AppIcons.quetions),
+            title: const Text(
+              'Frequently asked questions',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 0.0,
+          child: ListTile(
+            trailing: SvgPicture.asset(AppIcons.go),
+            leading: SvgPicture.asset(AppIcons.logout),
+            title: const Text(
+              'Logout',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
