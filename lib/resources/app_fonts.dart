62c2483582ea3eb27dde8b3b1f5292de7afb2ad8
Oleksandr <bounceit1221@gmail.com>
2022-11-06 12:53:25 +0300

    Create app_fonts.dart

---
+++ lib/resources/app_fonts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const String fontFamily = 'Inter';
+
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w800;
+  static const FontWeight heavy = FontWeight.w900;
+}
