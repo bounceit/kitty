92a37c3e5317914aa553c6b35a1df98a0b9ad5ff
Oleksandr <bounceit1221@gmail.com>
2022-11-12 13:07:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kitty/pages/splash_page/splash_page.dart';
 import 'package:kitty/resources/app_theme.dart';
 import 'package:kitty/routes/routes.dart';
+import 'package:month_year_picker/month_year_picker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +24,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        MonthYearPickerLocalizations.delegate,
+      ],
       theme: AppThemes.light(),
       initialRoute: SplashScreen.routeName,
       onGenerateRoute: AppRouter.generateRoute,
