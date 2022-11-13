6d95b2ca2f1378ce5a2dcbf19b601454a2656f7b
Oleksandr <bounceit1221@gmail.com>
2022-11-13 12:03:36 +0300

    Create total_bloc.dart

---
+++ lib/pages/main_pages/home_page/widgets/bloc/total_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:kitty/models/summary_model.dart';
+import 'package:kitty/repository/transaction_repository.dart';
+
+part 'total_event.dart';
+part 'total_state.dart';
+
+class TotalBloc extends Bloc<TotalEvent, TotalState> {
+  TotalBloc() : super(TotalInitial()) {
+    final KittyRepository kittyRep = KittyRepository();
+
+    on<GetTotalList>((event, emit) async {
+      try {
+        emit(TotalLoading());
+        final mList = await kittyRep.totalIncome();
+        final eList = await kittyRep.totalExpense();
+
+        emit(TotalLoaded(
+          summuryModel: mList.first,
+          summaryExp: eList.first,
+        ));
+      } on NetworkError {
+        emit(TotalError("Failed to fetch data. is your device online?"));
+      }
+    });
+  }
+}
