4d3093c90221cde1b477bf6cb2fb66551a7b2506
Oleksandr <bounceit1221@gmail.com>
2022-11-14 18:05:28 +0300

    Update search_page.dart

--- lib/pages/main_pages/search_page/search_page.dart
+++ lib/pages/main_pages/search_page/search_page.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:kitty/pages/main_pages/search_page/widgets/list_search_category.dart';
+import 'package:kitty/pages/main_pages/search_page/widgets/text_box.dart';
+import 'package:kitty/resources/app_colors.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
   static const routeName = '/search_page';
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  bool typing = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.grey.shade500),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        actions: [TextField()],
+        toolbarHeight: 250,
+        iconTheme: IconThemeData(color: Colors.grey.shade400),
+        backgroundColor: Colors.white,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Expanded(flex: 1, child: TextBox()),
+            Expanded(child: ListSearchCategory())
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('SearchPage'),
+      body: const Center(
+        child: const Text('Your app content'),
       ),
     );
   }
