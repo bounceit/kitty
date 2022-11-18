0a218f5ecbd51f775ab2539cfdad3b5c37a82e9c
Oleksandr <bounceit1221@gmail.com>
2022-11-18 11:20:59 +0200

    Update list_search_category.dart

--- lib/pages/main_pages/search_page/widgets/list_search_category.dart
+++ lib/pages/main_pages/search_page/widgets/list_search_category.dart
@@ -1,58 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kitty/models/category_model.dart';
+import 'package:kitty/models/transaction.dart';
 import 'package:kitty/repository/transaction_repository.dart';
+import 'package:kitty/resources/app_icons.dart';
 
-class ListSearchCategory extends StatelessWidget {
-  const ListSearchCategory({
+class ListSearchCategory extends StatefulWidget {
+  ListSearchCategory({
+    required this.isSelected,
+    required this.categorys,
     Key? key,
   }) : super(key: key);
+  ValueChanged<bool> isSelected;
+  List<CategoryModel> categorys;
+
+  @override
+  State<ListSearchCategory> createState() => _ListSearchCategoryState();
+}
+
+class _ListSearchCategoryState extends State<ListSearchCategory> {
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: KittyRepository().getAllCategory(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final List<CategoryModel> categorys =
-              snapshot.data as List<CategoryModel>;
-          if (categorys.isEmpty) {
-            return const Text('Добавьте категорию');
-          } else {
-            return Container(
-              child: ListView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categorys.length,
-                  itemBuilder: (context, index) {
-                    return Row(
+    return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.categorys.length,
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              Card(
+                elevation: 0.0,
+                borderOnForeground: false,
+                // color: Colors.transparent,
+                child: Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: isSelected
+                          ? Colors.blue
+                          : Color(widget.categorys[index].color),
+                      width: 3,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: AlignmentDirectional.center,
+                  child: TextButton(
+                    child: Row(
                       children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(150),
-                            color: Color(categorys[index].color),
-                          ),
-                          alignment: AlignmentDirectional.center,
-                          child: TextButton(
-                            child: SvgPicture.asset(
-                              categorys[index].icon,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ),
-                        Text(categorys[index].category)
+                        isSelected
+                            ? SvgPicture.asset(
+                                AppIcons.done,
+                              )
+                            : SvgPicture.asset(
+                                widget.categorys[index].icon,
+                              ),
+                        Spacer(),
+                        Text(
+                          widget.categorys[index].category,
+                          style: TextStyle(color: Colors.black),
+                        )
                       ],
-                    );
-                  }),
-            );
-          }
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isSelected = !isSelected;
+                        widget.isSelected(isSelected);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          );
         });
   }
 }
