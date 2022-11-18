e7081463aebb60fc319aeae4a7c4fa822be999dd
Oleksandr <bounceit1221@gmail.com>
2022-11-18 11:20:55 +0200

    Update search_page.dart

--- lib/pages/main_pages/search_page/search_page.dart
+++ lib/pages/main_pages/search_page/search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kitty/pages/main_pages/search_page/bloc/search_bloc.dart';
 import 'package:kitty/pages/main_pages/search_page/widgets/list_search_category.dart';
 import 'package:kitty/pages/main_pages/search_page/widgets/text_box.dart';
 import 'package:kitty/resources/app_colors.dart';
@@ -17,20 +19,66 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 250,
+        elevation: 0.0,
+        toolbarHeight: 50,
         iconTheme: IconThemeData(color: Colors.grey.shade400),
         backgroundColor: Colors.white,
-        title: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Expanded(flex: 1, child: TextBox()),
-            Expanded(child: ListSearchCategory())
-          ],
-        ),
+        title: const TextBox(),
       ),
-      body: const Center(
-        child: const Text('Your app content'),
+      body: BlocProvider(
+        create: (context) => SearchBloc()
+          ..add(
+            GetSearchList(),
+          ),
+        child: BlocListener<SearchBloc, SearchState>(
+          listener: (context, state) {
+            if (state is SearchError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message!),
+                ),
+              );
+            }
+          },
+          child: BlocBuilder<SearchBloc, SearchState>(
+            builder: (context, state) {
+              if (state is SearchInitial) {
+                return const CircularProgressIndicator();
+              } else if (state is SearchLoading) {
+                return const CircularProgressIndicator();
+              } else if (state is SearchLoaded) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: ListSearchCategory(
+                        categorys: state.transactionModel,
+                        isSelected: (bool value) {},
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Container(),
+                    )
+                  ],
+                );
+              } else if (state is SearchError) {
+                return Container();
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ),
       ),
     );
   }
