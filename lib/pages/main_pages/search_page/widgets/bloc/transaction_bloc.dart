798d1a22e8ebbf4f0ccd665bb978780bbec7a3b6
Oleksandr <bounceit1221@gmail.com>
2022-11-25 13:06:58 +0200

    Create transaction_bloc.dart

---
+++ lib/pages/main_pages/search_page/widgets/bloc/transaction_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:kitty/models/transaction.dart';
+import 'package:kitty/repository/transaction_repository.dart';
+
+part 'transaction_event.dart';
+part 'transaction_state.dart';
+
+class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
+  TransactionBloc() : super(const TransactionState()) {
+    // on<InitTransaction>((event, emit) async {
+    //   final getAll = await KittyRepository().getAlltranzaction();
+    //   emit(
+    //     state.copyWith(
+    //       transactions: getAll,
+    //     ),
+    //   );
+    // });
+    on<SearchTransaction>((event, emit) async {
+      final getAllsearch = await KittyRepository().search(event.text);
+      emit(
+        state.copyWith(
+          transactions: getAllsearch,
+        ),
+      );
+    });
+    on<FilterTransaction>((event, emit) {});
+  }
+}
