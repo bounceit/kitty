342d5e17bc7379f3a9185597e77575a76651a435
Oleksandr <bounceit1221@gmail.com>
2022-11-26 12:52:20 +0200

    Update container_for_list.dart

--- lib/pages/main_pages/home_page/widgets/container_for_list.dart
+++ lib/pages/main_pages/home_page/widgets/container_for_list.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kitty/models/summary_model.dart';
+import 'package:kitty/pages/main_pages/home_page/widgets/bloc/total_bloc.dart';
 import 'package:kitty/pages/main_pages/home_page/widgets/transaction_builder.dart';
+import 'package:kitty/pages/main_pages/search_page/widgets/bloc/transaction_bloc.dart';
 
 class ContainerForList extends StatelessWidget {
-  const ContainerForList({
+  ContainerForList({
     Key? key,
+    required this.child,
     required this.screenHeight,
     required this.screenWeight,
   }) : super(key: key);
-
+  Widget child;
   final double screenHeight;
   final double screenWeight;
   @override
@@ -19,6 +24,51 @@ class ContainerForList extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
           border: Border.all(width: 1.5, color: Colors.grey.shade300),
         ),
-        child: const TransactionBuilder());
+        child: child);
+  }
+}
+
+class TodayTransactionBuilder extends StatelessWidget {
+  TodayTransactionBuilder({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+  Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TotalBloc()..add(GetTotalList()),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Today',
+                  style: TextStyle(
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+                BlocBuilder<TotalBloc, TotalState>(
+                  builder: (context, state) {
+                    if (state is TotalLoaded) {
+                      return Text(
+                        state.summaryExp.expensive.toString(),
+                        style: TextStyle(color: Colors.grey.shade500),
+                      );
+                    }
+                    return Container();
+                  },
+                )
+              ],
+            ),
+          ),
+          Expanded(child: child),
+        ],
+      ),
+    );
   }
 }
