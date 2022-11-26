357532fb26eaa12e9cf6e2584719841bab553288
Oleksandr <bounceit1221@gmail.com>
2022-11-26 12:52:11 +0200

    Update main_of_main.dart

--- lib/pages/main_of_main.dart
+++ lib/pages/main_of_main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kitty/blocs/bloc_navigations/navigation_bloc.dart';
 import 'package:kitty/pages/main_pages/home_page/home_page.dart';
+import 'package:kitty/pages/main_pages/home_page/widgets/bloc/total_bloc.dart';
 import 'package:kitty/pages/main_pages/report_page/report_page.dart';
 import 'package:kitty/pages/main_pages/search_page/widgets/bloc/transaction_bloc.dart';
 import 'package:kitty/pages/main_pages/settings_page/settings_page.dart';
@@ -66,6 +67,9 @@ class _MainPageState extends State<MainPage> {
         ),
         BlocProvider<TransactionBloc>(
           create: (_) => TransactionBloc(),
+        ),
+        BlocProvider<TotalBloc>(
+          create: (_) => TotalBloc(),
         ),
       ],
       child: BlocConsumer<NavigationBloc, NavigationState>(
