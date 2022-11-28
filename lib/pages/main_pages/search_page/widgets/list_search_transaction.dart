fb82fb8c98ed8af7fb6c5df95e4845d62d67016a
Oleksandr <bounceit1221@gmail.com>
2022-11-28 17:39:02 +0200

    Update list_search_transaction.dart

--- lib/pages/main_pages/search_page/widgets/list_search_transaction.dart
+++ lib/pages/main_pages/search_page/widgets/list_search_transaction.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kitty/pages/main_pages/search_page/widgets/bloc/transaction_bloc.dart';
