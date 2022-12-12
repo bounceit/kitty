c55eec0128ed81af5660cd2ed586b983b613028e
Oleksandr <bounceit1221@gmail.com>
2022-12-12 16:08:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:kitty/blocs/bloc_navigations/navigation_bloc.dart';
 import 'package:kitty/pages/splash_page/splash_page.dart';
 import 'package:kitty/resources/app_theme.dart';
 import 'package:kitty/routes/routes.dart';
 import 'package:kitty/widgets/uncategorized/data_picker/bloc/main_screen_bloc.dart';
 import 'package:month_year_picker/month_year_picker.dart';
+
+import 'pages/main_pages/home_page/widgets/bloc/total_bloc.dart';
+import 'pages/main_pages/search_page/widgets/bloc/transaction_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +31,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<MainScreenBloc>(create: (context) => MainScreenBloc()),
+        BlocProvider<MainScreenBloc>(
+          create: (_) => MainScreenBloc()
+            ..add(GetMonthEvent())
+            ..add(GetTapMounthEvent()),
+        ),
+        BlocProvider<NavigationBloc>(
+          create: (_) => NavigationBloc(),
+        ),
+        BlocProvider<TransactionBloc>(
+          create: (_) => TransactionBloc(),
+        ),
+        BlocProvider<TotalBloc>(
+          create: (_) => TotalBloc(),
+        ),
       ],
       child: MaterialApp(
         localizationsDelegates: const [
